2b79ca0229bfb7fffaaed7dd32d552e00ef926eb
ksks1986 <32826518+ksks1986@users.noreply.github.com>
2018-09-15 16:25:22 +0900

    submit1

---
+++ build/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_4440a.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_4440a.dir/feature_tests.cxx.o"
+  "cmTC_4440a.pdb"
+  "cmTC_4440a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_4440a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
